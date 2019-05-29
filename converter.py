fed85a42e15d7f3b0dfd93c313223f026386c535
Mitchell <minimitch0199@gmail.com>
2019-05-29 21:28:22 +1000

    Clean up

--- converter.py
+++ converter.py
@@ -13,16 +13,16 @@ from converter import entity as Entity
 from converter import tileEntity as TileEntity
 from converter import util as Util
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 
 def save_chunk(region, chunk):
     region.write_chunk(chunk.x, chunk.z, chunk)
-    
+
 def save_level(level, world_folder):
     level["Data"].__delitem__("Version")
     level.write_file(os.path.join(world_folder, "level.dat"))
 
-def convert_chunk(chunk, version):
+def convert_chunk(chunk):
     nbt = chunk["Level"]
     edits = 0
     if (len(nbt["Entities"]) > 0) or (len(nbt["TileEntities"]) > 0):
@@ -39,34 +39,38 @@ def convert_block(chunk):
 
 def main(world_folder, options):
     world = WorldFolder(world_folder)
+
     if not isinstance(world, nbt.world.AnvilWorldFolder):
-        parser.error("%s is not an Anvil world" % (world_folder))
+        print(world_folder + " is not an Anvil world")
+        return 0
+
     level = NBTFile(os.path.join(world_folder, "level.dat"))
     version = Util.get_version(level)
-    print("\nConverting world located at " + world_folder)
-    print("Level saved as Minecraft version " + version)
+
+    print("\nLoading level at " + world_folder)
     if version != "1.8":
+        print("Level saved as Minecraft version " + version)
         try:
             total_nbt_edits = 0
             total_block_edits = 0
             for region in world.iter_regions():
+                print("Iterating chunks in " + region.filename.rsplit('\\', 1)[1])
                 for chunk in region.iter_chunks():
-                    chunk, nbt_edits = convert_chunk(chunk, version)
+                    chunk, nbt_edits = convert_chunk(chunk)
                     chunk, block_edits = convert_block(chunk)
                     total_nbt_edits += nbt_edits
                     total_block_edits += block_edits
                     if nbt_edits > 0 or block_edits > 0: save_chunk(region, chunk)
             if (total_nbt_edits > 0) or (total_block_edits > 0):
-                if total_nbt_edits > 0: print("%d modifications made to %s" % (total_nbt_edits, world_folder))
+                if total_nbt_edits > 0: print("%d modifications made to the level nbt" % (total_nbt_edits))
                 if total_block_edits > 0: print("%d modifications made to block section byte arrays" % (total_block_edits))
                 save_level(level, world_folder)
             else:
                 print("No NBT data was changed")
-
         except KeyboardInterrupt:
             return 75
     else:
-        print("World is already for Minecraft 1.8")
+        print("Level is already saved for Minecraft 1.8 (or older)")
 
     return 0
 
@@ -84,7 +88,7 @@ if __name__ == "__main__":
     directory = os.path.normpath(directory)
     if (not os.path.exists(directory)):
         parser.error("No such folder as " + directory)
-        
+
     if (options.recursive == True):
         for dirpath, dirnames, filenames in os.walk(directory):
             if "region" in dirnames and "level.dat" in filenames:
