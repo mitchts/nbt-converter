af689ee86466336ac0b14396964756434963ba6d
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2026-01-26 18:27:57 +1100

    Add flag to discard chunk version data

--- converter.py
+++ converter.py
@@ -8,17 +8,19 @@ import os, sys, nbt
 from optparse import OptionParser
 from nbt.world import WorldFolder
 from nbt.nbt import NBTFile
-from converter import block as Block
-from converter import entity as Entity
-from converter import tileEntity as TileEntity
-from converter import util as Util
+from conversions import block as Block
+from conversions import entity as Entity
+from conversions import tileEntity as TileEntity
+from conversions import util as Util
 
 VERSION = "1.3.2"
 
-def save_chunk(region, chunk):
+def save_chunk(region, chunk, announce):
     # Discard data version to avoid issues when upgrading the world back
     if chunk.__contains__("DataVersion") and chunk["DataVersion"].value != 0:
         chunk["DataVersion"].value = 0
+        if announce:
+            print("Discarding chunk version")
     region.write_chunk(chunk.x, chunk.z, chunk)
 
 def save_level(level, world_folder):
@@ -62,7 +64,7 @@ def main(world_folder, options):
     version = Util.get_version(level)
 
     print("\nLoading level at " + world_folder)
-    if version != "1.8" or options.force:
+    if version != "1.8" or options.force or options.discard_chunk_version:
         if options.force:
             print("[Forcing level conversion attempt]")
         print("Level saved as Minecraft version " + version)
@@ -71,13 +73,15 @@ def main(world_folder, options):
             total_block_edits = 0
             for region in world.iter_regions():
                 for chunk in region.iter_chunks():
+                    nbt_edits = 0
+                    block_edits = 0
                     if not options.discard_chunk_version or options.force:
                         chunk, nbt_edits = convert_chunk(chunk)
                         chunk, block_edits = convert_block(chunk)
                         total_nbt_edits += nbt_edits
                         total_block_edits += block_edits
                     if options.save and (nbt_edits > 0 or block_edits > 0 or options.discard_chunk_version):
-                        save_chunk(region, chunk)
+                        save_chunk(region, chunk, options.discard_chunk_version)
             if total_nbt_edits > 0 or total_block_edits > 0:
                 print("%d modifications made to the level nbt" % (total_nbt_edits))
                 print("%d modifications made to block section byte arrays" % (total_block_edits))
